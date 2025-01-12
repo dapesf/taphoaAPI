cf32267121dd59c8d351f50ceae57b29af359fd1
daPesf <cd15km1@gmail.com>
2025-01-13 00:41:37 +0700

    update product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using InterFace;
+using Microsoft.OData.ModelBuilder;
 
 namespace App.Controllers;
 
@@ -19,24 +21,46 @@ public class ProductController : ControllerBase
     [HttpGet]
     public IActionResult GetProduct(string cd_product, string cd_store)
     {
-        tr_product? tr_anounce = context.tr_product.Find([cd_product, cd_store]);
-        return NotFound(new
-        {
-            status = "Get Product sucessfull!",
-            data = tr_anounce
-        });
+        tr_product? tr_product = context.tr_product.Find([cd_product, cd_store]);
+        return Ok(new ResponseResult
+        (
+            "I200",
+            "Get Product sucessfull!",
+            tr_product
+        ));
     }
 
     [HttpGet]
     public IActionResult GetAllProduct()
     {
-        List<tr_product> tr_products = new List<tr_product>();
-        tr_products = context.tr_product.ToList();
-        return Ok(new
+        var products = from product in context.tr_product
+        join unit in (context.ma_literal.Where(x => x.cd_type == "001"))
+        on product.type_unit equals unit.kbn1
+        into dataGroup
+        from data in dataGroup.DefaultIfEmpty()
+        orderby product.cd_product descending
+        select new ProductResponse
         {
-            status = "Post Product sucessfull!",
-            data = tr_products
-        });
+            cd_product = product.cd_product
+            , cd_store = product.cd_store
+            , nm_product = product.nm_product
+            , nm_product_en = product.nm_product_en
+            , dt_start = product.dt_start
+            , dt_end = product.dt_end
+            , kin_price = product.kin_price
+            , type_unit = product.type_unit
+            , qnt_in = product.qnt_in
+            , qnt_remain = product.qnt_remain
+            , cd_country = product.cd_country
+            , unit = data.nm1
+        };
+
+        return Ok(new ResponseResult
+        (
+            "I200",
+            "Get Products sucessfull!",
+            products
+        ));
     }
 
     [HttpPost]
@@ -89,4 +113,20 @@ public class ProductController : ControllerBase
             });
         }
     }
+}
+
+public class ProductResponse
+{
+    public string cd_product { get; set; }
+    public string cd_store { get; set; }
+    public string nm_product { get; set; }
+    public string nm_product_en { get; set; }
+    public DateOnly dt_start { get; set; }
+    public DateOnly dt_end { get; set; }
+    public double kin_price { get; set; }
+    public string type_unit { get; set; }
+    public int qnt_in { get; set; }
+    public int qnt_remain { get; set; }
+    public string cd_country { get; set; }
+    public string unit { get; set; }
 }
